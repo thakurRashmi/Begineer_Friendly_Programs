4ae3790a10ddedd0294a6b136edd0cbf1952c9d1
Kumar Raunak <115032346+raunakthakur1909@users.noreply.github.com>
2022-10-08 10:46:14 +0530

    Create Differenceof2numbers.cpp
    This is C++ program which takes two inputs from the user and return the absolute difference between them.

---
+++ Differenceof2numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int  a;
+    int  b;
+    int difference;
+
+    cout << "Enter a Number  \n";
+    cin  >>  a;
+
+    cout  << "Enter Another Number \n";
+    cin   >> b;
+
+     difference = a -b ;
+    cout  << "the difference of those numbers is"  <<  difference << endl;
+
+     return 0;
+}
