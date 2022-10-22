75c4c8ab5979dfbdc01c876b18e6dc1c2e237ec3
Vaibhav Chaudhary <83056597+PerksofbeingVaibhav@users.noreply.github.com>
2022-10-22 08:34:23 +0530

    first commit
    leetcode PROBLEM : group anagrams

---
+++ group_anagrams.cpp
@@ -0,0 +1,17 @@
+ vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+          vector<vector<string>>ans;
+          unordered_map<string,vector<string>>mp;
+        
+          for(int i=0;i<strs.size();i++){
+              string s=strs[i];
+              sort(s.begin(),s.end());
+              mp[s].push_back(strs[i]);
+          }
+        
+        for(auto it : mp){
+            ans.push_back(it.second);
+        }
+        
+        return ans;
+    }
